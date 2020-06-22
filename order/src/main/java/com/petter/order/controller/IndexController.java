d03a8b36b4e55da700b9a086164bb4602c212707
GD <455607047@qq.com>
2020-06-22 20:20:04 +0800

    nacos 工程修改

--- order/src/main/java/com/petter/order/controller/IndexController.java
+++ order/src/main/java/com/petter/order/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.petter.order.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("page")
 public class IndexController {
 
+    private static int count = 0;
+
     @GetMapping("index")
+    @SentinelResource("index")
     public String index(){
-        return "nacos index page ";
+        System.out.println("index count ========================= " + count++);
+        return "nacos index page " + count;
+    }
+
+    @GetMapping("resetCount")
+    @SentinelResource("resetCount")
+    public String resetCount(){
+        System.out.println("reset count " + count);
+        int temp = count;
+        count = 0;
+        return "nacos index page " + temp;
     }
 
 }
